05b34b5a832eed5753a8c6d5d9378db08b7372a6
urbanmarovt <marovt.urban@gmail.com>
2017-10-21 20:08:52 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export const HTTP = axios.create({
+  baseURL: `http://localhost:49160/`,
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  }
+});
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  created() {
+    HTTP.get('/')
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.msg = response.data
+    })
+  }
+}
+</script>
+
+<style{{#sass}} lang="scss"{{/sass}}>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
